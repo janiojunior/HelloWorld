7e74ce673258179e7289c017468de9befa6d3d23
Janio <janiojunior@gmail.com>
2020-02-16 18:22:04 -0300

    Hello world JSF

---
+++ src/controller/LoginController.java
@@ -0,0 +1,34 @@
+package controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class LoginController {
+
+	private String login = "Maria";
+	private String senha;
+	
+	public void entrar() {
+		System.out.println(login);
+		System.out.println(senha);
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
